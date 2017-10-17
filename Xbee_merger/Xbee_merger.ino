8326948abfdd9e95c8071c06c0c790e337df71b1
Chikashi Miyama <chikashimiyama@gmail.com>
2017-10-17 14:06:49 +0200

    prototype

---
+++ Xbee_merger/Xbee_merger.ino
@@ -0,0 +1,19 @@
+
+const int BAUD = 115200
+
+void setup() {
+  Serial.begin(BAUD); // to computer
+  Serial1.begin(BAUD); // to XBEE1
+  Serial2.begin(BAUD); // to XBEE2
+}
+
+void loop() {
+  
+  if (Serial1.available()) { 
+    Serial.write(Serial.read());
+  }
+
+  if (Serial2.available()) {
+    Serial.write(Serial2.read());
+  }
+}
