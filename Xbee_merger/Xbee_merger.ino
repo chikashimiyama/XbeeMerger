a2a11c21e1c76e7daff51a6c797ac236c08a78c4
Chikashi Miyama <chikashimiyama@gmail.com>
2018-06-16 18:07:17 +0000

    Update 'Xbee_merger/Xbee_merger.ino'

--- Xbee_merger/Xbee_merger.ino
+++ Xbee_merger/Xbee_merger.ino
@@ -1,19 +1,29 @@
+const int BAUD = 115200;
+const int PORT0 = 0;
+const int PORT1 = 1;
 
-const int BAUD = 115200
+// simple USART merger
+// it just checks hardware serial 2 and 3 and forwards them to Serial port
+// the MSB is the ID that indicates the source
 
 void setup() {
   Serial.begin(BAUD); // to computer
-  Serial1.begin(BAUD); // to XBEE1
-  Serial2.begin(BAUD); // to XBEE2
+  Serial2.begin(BAUD); // to XBEE1
+  Serial3.begin(BAUD); // to XBEE2
 }
 
 void loop() {
   
-  if (Serial1.available()) { 
-    Serial.write(Serial.read());
-  }
-
-  if (Serial2.available()) {
+  while (Serial2.available()) { 
     Serial.write(Serial2.read());
   }
+  
+  
+  while (Serial3.available()) {
+    uint8_t val = Serial3.read();
+    val |= 128; // identifier
+    Serial.write(val);
+  }
+
+  delay(5);
 }
